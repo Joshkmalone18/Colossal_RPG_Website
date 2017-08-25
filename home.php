b6bc500c3661655f17be19a044a6d60720899ee3
Joshkmalone18 <31298295+Joshkmalone18@users.noreply.github.com>
2017-08-25 14:29:12 -0700

    Update home.php

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 <head>
 <title>Colossal RPG</title>
 <style>
-  body { color: #3f312e; background: #77d5f9; font: 14px Helvetica, Arial, Sans0serif; } 
+  body { color: #3f312e; background: #77d5f9; font: 14px Helvetica, Arial, Sans-serif; } 
   #header { width: 960px; min-height: 150px; margin: 0 auto; }
   #navigation { width: 960px; margin: 0 auto; }
   #container { overflow: auto; width: 960px; margin: 0 auto; }
