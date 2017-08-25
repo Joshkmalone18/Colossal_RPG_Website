e5efd73e1543566553e341caccbdd96fd88ef208
Joshkmalone18 <31298295+Joshkmalone18@users.noreply.github.com>
2017-08-25 14:29:01 -0700

    Update home.php

--- home.php
+++ home.php
@@ -2,32 +2,41 @@
 <head>
 <title>Colossal RPG</title>
 <style>
-
+  body { color: #3f312e; background: #77d5f9; font: 14px Helvetica, Arial, Sans0serif; } 
+  #header { width: 960px; min-height: 150px; margin: 0 auto; }
+  #navigation { width: 960px; margin: 0 auto; }
+  #container { overflow: auto; width: 960px; margin: 0 auto; }
+  #content {width: 960px; min-height: 400px; margin: 0 auto; background: #fff; }
+  #contentleft { width: 580px; float: left; margin-right: 20px: }
+  #sidebar { width: 280px; float: left; }
+  #footer { width: 960px; margin: 0 auto; }
 </style>
 </head>
 <body>
 <div id="header">
 
-</div>
+</div><!--header-->
 
 <div id="navigation">
 
-</div>
+</div><!--navigation-->
 
 <div id="container">
 <div id="content">
 <div id="contentleft">
 
-</div>
+</div><!--contentleft-->
 
 <div id="sidebar">
 
-</div>
-</div>
-<div id="footer">
+</div><!--sidebar-->
+  
+</div><!--content-->
+</div><!--container-->
 
-</div>
-</div>
+<div id="footer">
+  
+</div><!--footer-->
 
 
 
