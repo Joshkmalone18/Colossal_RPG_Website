4f90a4eadfbd638f8cd5e6fb4928c543eb71bbd2
Joshkmalone18 <31298295+Joshkmalone18@users.noreply.github.com>
2017-08-25 14:11:45 -0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>Colossal RPG</title>
+<style>
+
+</style>
+</head>
+<body>
+<div id="header">
+
+</div>
+
+<div id="navigation">
+
+</div>
+
+<div id="container">
+<div id="content">
+<div id="contentleft">
+
+</div>
+
+<div id="sidebar">
+
+</div>
+</div>
+<div id="footer">
+
+</div>
+</div>
+
+
+
+
+</body>
+
+</head>
+</html>
