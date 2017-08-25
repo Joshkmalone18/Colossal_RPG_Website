87d934ffd1534d7031981f75123ec614f62e133d
Joshkmalone18 <31298295+Joshkmalone18@users.noreply.github.com>
2017-08-25 15:47:43 -0700

    Update index.php

--- index.php
+++ index.php
@@ -18,24 +18,42 @@
 </div><!--header-->
 
 <div id="navigation">
+  <ul>
+    <li><a href="">Home</a></li>
+    <li><a href="">Miniatures</a></li>
+    <li><a href="">Contact</a></li>
+  </ul>
 
 </div><!--navigation-->
 
 <div id="container">
 <div id="content">
 <div id="contentleft">
-
+  <h1> Welcome to Colossal RPG</h1>
+  <p>Colossal RPG is a small buisness based in Portland Oregon. We make everything you need for your table.</p>
+  <p>We sell a variety of things for both DMs and players such as Paper Miniatures, Spell Cards, and Dice Boxes.</p>
+  <p>If your intrested in anything please Email us at Colossalrpg@gmail.com!
 </div><!--contentleft-->
 
 <div id="sidebar">
-
+<div class="sidebar-element">  
+  <h3>About Me</h3>
+  <p>I'm a local artist who simply loves playing boardgames, specifically Dungeons and Dragons.</p>
+  <p>I started drawing Miniatures a local table that I run and people began to ask where I had bought them, and just like that Colossal RPG was born</p>
+  <p>Since then I've branched out into other things that everyone may need for their game.
+  </div><!--Sidebar-elemnet-->
 </div><!--sidebar-->
   
 </div><!--content-->
 </div><!--container-->
 
 <div id="footer">
-  
+  <ul>
+    <li><a href="">Home</a></li>
+    <li><a href="">Miniatures</a></li>
+    <li><a href="">Contact</a></li>
+  </ul>
+  <p>&copy;2017 colossalrpg.com</p>
 </div><!--footer-->
 
 
